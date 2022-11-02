e2d6aa6cf2f599088404ad4e7ccf5d55b757728f
shonhartman <shaun@bighuman.com>
2022-11-02 18:05:44 -0400

    adds example from webflow

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import './styles';
+import './scripts';
+import IndexView from './views/IndexView';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <IndexView />
   </React.StrictMode>,
   document.getElementById('root')
 );
